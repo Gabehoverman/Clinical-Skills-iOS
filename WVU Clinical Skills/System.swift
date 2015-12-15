7defce70e6a02beaff0683e0e14303043bfc5e68
= <=>
2015-12-14 23:14:18 -0500

    Create System data model

---
+++ WVU Clinical Skills/System.swift
@@ -0,0 +1,19 @@
+//
+//  System.swift
+//  WVU Clinical Skills
+//
+//  Created by Nick on 12/14/15.
+//  Copyright © 2015 Nick. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+
+@objc(System)
+class System: NSManagedObject {
+	
+	@NSManaged var systemName: String
+	@NSManaged var systemDescription : String
+	
+}
