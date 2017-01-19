9bf80816883f83c991e920b6fe16d2c5f5a4a25b
matt <taylor607@marshall.edu>
2017-01-18 23:36:50 -0500

    Preparing for app store update

--- Clinical Skills/Controller/DisclaimerViewController.swift
+++ Clinical Skills/Controller/DisclaimerViewController.swift
@@ -9,7 +9,58 @@
 import Foundation
 import UIKit
 
-
 class DisclaimerViewController : UIViewController {
     
+    // MARK: - Properties
+    
+    var documentController: UIDocumentInteractionController?
+    var dismissing: Bool?
+    
+    // MARK: - View Controller Methods
+    
+    override func viewDidLoad() {
+        if let url = Bundle.main.url(forResource: "Disclaimer", withExtension: "pdf") {
+            self.documentController = UIDocumentInteractionController(url: url)
+            self.documentController!.delegate = self
+            self.dismissing = false
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.presentPreview()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.dismissing = false
+    }
+    
+    // MARK: - Interface Builder Connections
+    
+    @IBAction func show(_ sender: UIButton) {
+        self.dismissing = false
+        self.presentPreview()
+    }
+    
+    // MARK: - Navigation Methods
+    
+    func presentPreview() {
+        if let dismissing = self.dismissing, dismissing == false {
+            if self.documentController != nil {
+                self.documentController!.presentPreview(animated: true)
+            }
+        }
+    }
+    
+}
+
+// MARK: - Document Interface Controller Delegate Methods
+
+extension DisclaimerViewController : UIDocumentInteractionControllerDelegate {
+    func documentInteractionControllerViewControllerForPreview(_ controller: UIDocumentInteractionController) -> UIViewController {
+        return self.navigationController!
+    }
+    
+    func documentInteractionControllerDidEndPreview(_ controller: UIDocumentInteractionController) {
+        self.dismissing = true
+    }
 }
